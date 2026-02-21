e6a9d3ede2128d8bdbc958c780f4fd1dfa2e6c40
EcchiGrill <dmitry.oborsky@gmail.com>
2026-02-21 15:28:23 +0200

    :sparkles: Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { routeTree } from './routeTree.gen';
+
+const router = createRouter({ routeTree });
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const rootElement = document.getElementById('root')!;
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(<RouterProvider router={router} />);
---
+++ src/providers/ThemeProvider.tsx
@@ -0,0 +1,16 @@
+import type { PropsWithChildren } from 'react';
+import {
+  createTheme,
+  ThemeProvider as MUIThemeProvider,
+} from '@mui/material/styles';
+import { defaultPalette } from '../styles/palette';
+import { defaultTypography } from '../styles/typography';
+
+export const ThemeProvider = ({ children }: PropsWithChildren) => {
+  const defaultTheme = createTheme({
+    palette: defaultPalette,
+    typography: defaultTypography,
+  });
+
+  return <MUIThemeProvider theme={defaultTheme}>{children}</MUIThemeProvider>;
+};
